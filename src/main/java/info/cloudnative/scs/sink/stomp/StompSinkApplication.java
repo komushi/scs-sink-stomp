49616d69cc31ec86f9cb7a3e6d062c1fe1c71e5d
Lei Xu <komushi@gmail.com>
2017-12-17 21:22:22 +0900

    init

---
+++ src/main/java/info/cloudnative/scs/sink/stomp/StompSinkApplication.java
@@ -0,0 +1,12 @@
+package info.cloudnative.scs.sink.stomp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StompSinkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StompSinkApplication.class, args);
+	}
+}
---
+++ src/main/java/info/cloudnative/scs/sink/stomp/StompSinkProperties.java
@@ -0,0 +1,54 @@
+package info.cloudnative.scs.sink.stomp;
+
+/**
+ * Created by lei_xu on 6/26/16.
+ */
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+
+
+@ConfigurationProperties(prefix="stomp")
+public class StompSinkProperties {
+
+    /**
+     * The Websocket Path on which a StompSink consumer needs to connect
+     */
+    private String endpoint = "stomp";
+
+    /**
+     * The topic to subscribe/publish
+     */
+    private String topic;
+
+    /**
+     * Whether to use SockJS
+     */
+    private Boolean withsockjs = false;
+
+
+    public String getEndpoint() {
+        return endpoint;
+    }
+
+    public void setEndpoint(String endpoint) {
+        this.endpoint = endpoint;
+    }
+
+
+    public String getTopic() {
+        return topic;
+    }
+
+    public void setTopic(String topic) {
+        this.topic = topic;
+    }
+
+    public Boolean getWithsockjs() {
+        return withsockjs;
+    }
+
+    public void setWithsockjs(Boolean withsockjs) {
+        this.withsockjs = withsockjs;
+    }
+}
